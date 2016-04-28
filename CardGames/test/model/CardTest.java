7f20677c8c7b1ad5887b661cd620bf03ccd1742c
jens <jenspreem@gmail.com>
2016-04-28 16:06:45 +0300

    CardTest

--- CardGames/test/model/CardTest.java
+++ CardGames/test/model/CardTest.java
@@ -1,29 +1,65 @@
 package model;
 
 import static org.junit.Assert.*;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static model.Card.Face.*;
+import org.junit.Before;
 import org.junit.Test;
+import model.Card.Face;
+import model.Card.Suit;
+import static model.Card.Suit.*;
+
 
 public class CardTest {
+	List<Card> cardlist = new ArrayList<Card>();
+	List<Face> faces = new ArrayList<Face>(Arrays.asList(ACE,EIGHT,NINE,TWO,KING));
+	List<Suit> suits = new ArrayList<Suit>(Arrays.asList(SPADES,HEARTS,SPADES,DIAMONDS,CLUBS));
+
+	
+	@Before
+	public void initialize(){
+		
+		for (int i = 0; i < faces.size(); i++){
+			cardlist.add(i, new Card(faces.get(i),suits.get(i)));
+		}
+
+	}
 
 	@Test
 	public void testGetFace() {
-		fail("Not yet implemented");
+		
+		for (int i = 0; i < faces.size(); i++){
+			assertEquals(cardlist.get(i).getFace(),faces.get(i));
+
+		}
+
 	}
 
 	@Test
 	public void testSetFace() {
-		fail("Not yet implemented");
+		cardlist.get(0).setFace(FIVE);
+		assertEquals(cardlist.get(0).getFace(),FIVE);
+
 	}
 
 	@Test
 	public void testGetSuit() {
-		fail("Not yet implemented");
+		for (int i = 0; i < suits.size(); i++){
+			assertEquals(cardlist.get(i).getSuit(),suits.get(i));
+
+		}
 	}
 
 	@Test
 	public void testSetSuit() {
-		fail("Not yet implemented");
+		cardlist.get(0).setSuit(HEARTS);
+		assertEquals(cardlist.get(0).getSuit(),HEARTS);
+		cardlist.get(0).setSuit(DIAMONDS);
+		assertEquals(cardlist.get(0).getSuit(),DIAMONDS);
+
+	
 	}
 
 	@Test
@@ -38,7 +74,11 @@ public class CardTest {
 
 	@Test
 	public void testCompareTo() {
-		fail("Not yet implemented");
+		assert(cardlist.get(0).compareTo(cardlist.get(1))>0);
+		assertEquals(cardlist.get(1).compareTo(cardlist.get(1)),0);
+		assert(cardlist.get(1).compareTo(cardlist.get(4))<0);
+
+		
 	}
 
 }
